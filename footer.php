0474ce324f7746d8430b94f5ce1ba1d245eea8ba
Nadia <nadia.mohamed89@gmail.com>
2022-03-15 13:53:21 +0100

    Customize footer

--- footer.php
+++ footer.php
@@ -16,26 +16,48 @@
 <footer>
 	<div class="footer-copyright-area">
 		<div class="container container-1360">
-			<div class="row align-items-center">
-				<div class="col-lg-6 col-12">
-					<div class="social-links">
-						<ul>
-							<li class="title">Follow Me</li>
-							<li><a href="#">Twitter</a></li>
-							<li><a href="#">Facebook</a></li>
-							<li><a href="#">Youtube</a></li>
-							<li><a href="#">Instagram</a></li>
-							<li><a href="#">Linkedin</a></li>
-						</ul>
+			<?php
+			$args = array(
+				'role'    => 'administrator',
+				'orderby' => 'user_nicename',
+				'order'   => 'ASC'
+			);
+			$users = get_users($args);
+			foreach ($users as $user) { ?>
+				<div class="row align-items-center">
+					<div class="col-lg-6 col-12">
+						<div class="social-links">
+
+							<ul>
+								<?php if (get_field('author_twitter', 'user_' . $user->ID) && get_field('author_facebook', 'user_' . $user->ID) && get_field('author_youtube', 'user_' . $user->ID) && get_field('author_instagram', 'user_' . $user->ID) && get_field('author_linkedin', 'user_' . $user->ID)) { ?>
+									<li class="title">Follow Me</li>
+									<?php if (get_field('author_twitter', 'user_' . $user->ID)) { ?>
+										<li><a href="<?php echo get_field('author_twitter', 'user_' . $user->ID); ?>">Twitter</a></li>
+									<?php } ?>
+									<?php if (get_field('author_facebook', 'user_' . $user->ID)) { ?>
+										<li><a href="<?php echo get_field('author_facebook', 'user_' . $user->ID); ?>">Twitter</a></li>
+									<?php } ?>
+									<?php if (get_field('author_youtube', 'user_' . $user->ID)) { ?>
+										<li><a href="<?php echo get_field('author_youtube', 'user_' . $user->ID); ?>">Twitter</a></li>
+									<?php } ?>
+									<?php if (get_field('author_instagram', 'user_' . $user->ID)) { ?>
+										<li><a href="<?php echo get_field('author_instagram', 'user_' . $user->ID); ?>">Twitter</a></li>
+									<?php } ?>
+									<?php if (get_field('author_linkedin', 'user_' . $user->ID)) { ?>
+										<li><a href="<?php echo get_field('author_linkedin', 'user_' . $user->ID); ?>">Twitter</a></li>
+									<?php } ?>
+								<?php } ?>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6 col-12">
+						<div class="copyright-text text-lg-right">
+							<p><span>Copyright</span> - <?php echo date("Y"); ?> <?php echo $user->display_name; ?></p>
+						</div>
 					</div>
 				</div>
-				<div class="col-lg-6 col-12">
-					<div class="copyright-text text-lg-right">
-						<p><span>Copyright</span> - 2022 Nadia Mohamed</p>
-					</div>
-				</div>
-			</div>
 		</div>
+	<?php } ?>
 	</div>
 </footer>
 <!--====== Footer Area End ======-->
