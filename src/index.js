222e1bca870f757b5c2c6887331b6c1abf0a74a7
Nadia <nadia.mohamed89@gmail.com>
2022-03-25 19:31:03 +0100

    Add embeded data

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const pageNum = postsNum / 6;
 
 const getPostsData = async (page) => {
     try {
-        const { data } = await axios(`http://tech-blog.local/wp-json/wp/v2/posts?per_page=6&page=${page}`);
+        const { data } = await axios(`http://tech-blog.local/wp-json/wp/v2/posts?_embed=1&per_page=6&page=${page}`);
         return data;
     } catch (e) {
         console.log(e);
@@ -38,14 +38,19 @@ const displayPosts = (postsData) => {
     postsData.map(post => {
         let title = post.title.rendered;
         let date = post.date;
-        let categories = post.categories;
+        let categories = post._embedded[ 'wp:term' ][0];
         let excerpt = post.excerpt.rendered;
         let link = post.link;
+        let d = new Date(date);
+        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+        let mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+        let newDate = (`${mo} ${da}, ${ye}`);
 
         let postElem = `<div class="col-xl-4 col-sm-6" >
                             <div class="entry-post">
                                 <div class="entry-thumbnail">
-                                    <img src="" alt="Image" />
+                                    <img src="${post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url}" alt="Image" />
                                 </div>
                                 <div class="entry-content">
                                     <h4 class="title">
@@ -55,10 +60,12 @@ const displayPosts = (postsData) => {
                                     </h4>
                                     <ul class="post-meta">
                                         <li class="date">
-                                            ${date}
+                                            ${newDate}
                                         </li>
                                         <li class="categories">
-                                            <a href="/category/">${categories}</a>
+                                            ${categories.map(category => {
+                                                return `<a href="/category/${category.slug}">${category.name}</a>`;
+                                            }).join(" ")}
                                         </li>
                                     </ul>
                                     <p>
